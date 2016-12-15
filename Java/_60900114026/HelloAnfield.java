15fcff78f51b483f3b649b0285d513d35d81a2dc
MuhammadSyukran <syukrankomdas14@gmail.comgit config --global user.email syukrankomdas14@gmail.com>
2016-12-15 14:11:59 +0800

    versi 1.0.0

---
+++ Java/_60900114026/HelloAnfield.java
@@ -0,0 +1,14 @@
+package _60900114026;
+
+public class HelloAnfield{
+
+	public void sayHello(){
+		System.out.println("Halo Anfield......");
+		System.out.println("Halo");
+	}
+	
+	public static void main(String[] args){
+		HelloAnfield ha = new HelloAnfield();
+		ha.sayHello();
+	}	
+}
